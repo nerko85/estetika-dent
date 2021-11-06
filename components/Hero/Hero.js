87d34c844b079a13d423c6627e2df84f0482160e
nerko85 <31403849+nerko85@users.noreply.github.com>
2021-11-06 01:37:01 +0100

    quote change

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -14,9 +14,9 @@ export default function Hero({ children, title, desc, img }) {
           <img src={img} alt="Mature lady showing her theets" />
           <div className="quote">
             <blockquote>
-              ""Od danas se više ne bojim ići kod zubara jer zubari vole malu djecu""
+              "Od danas se više ne bojim ići kod zubara jer zubari vole malu djecu""
             </blockquote>
-            <span>Aleksandar Kovač</span>
+            <span>Dejan Petrović</span>
           </div>
         </div>
       </div>
